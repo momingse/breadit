01fbb794a8a3fbdc79cac7ae6d7feb6c8c97c1d1
momingse <da1710080850@gmail.com>
2023-08-22 16:21:15 +0800

    5.01.55

--- src/app/r/[slug]/layout.tsx
+++ src/app/r/[slug]/layout.tsx
@@ -3,6 +3,8 @@ import { db } from "@/lib/db";
 import { notFound } from "next/navigation";
 import { format } from "date-fns";
 import SubscribeLeaveToggle from "@/components/SubscribeLeaveToggle";
+import { buttonVariants } from "@/components/ui/Button";
+import Link from "next/link";
 
 const Layout = async ({
   children,
@@ -87,6 +89,16 @@ const Layout = async ({
                   isSubscribed={isSubscripted}
                 />
               ) : null}
+
+              <Link
+                className={buttonVariants({
+                  variant: "outline",
+                  className: "w-full mb-6",
+                })}
+                href={`r/${slug}/submit`}
+              >
+                Create Post
+              </Link>
             </dl>
           </div>
         </div>
